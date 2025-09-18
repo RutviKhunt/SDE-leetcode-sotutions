eae60015944ce467f41eb7404670cdcc00534a5e
RutviKhunt <rutvikhunt2023@gmail.com>
2025-09-18 23:22:30 +0530

    solution of roman to integer!!

---
+++ code8.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_map = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+     }
+    
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            # If this numeral is smaller than the next one → subtract
+            if i + 1 < n and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+        
+        return total
