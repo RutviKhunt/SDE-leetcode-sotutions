ac8385b5688e7a56906e9fac4b2b9e0ab4ed7720
RutviKhunt <rutvikhunt2023@gmail.com>
2025-11-10 17:00:43 +0530

    solution of next permutation problem

---
+++ code19.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        i = n - 2
+
+        # Step 1: Find first decreasing element from right
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        if i >= 0:
+            # Step 2: Find element just larger than nums[i]
+            j = n - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            # Step 3: Swap
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # Step 4: Reverse the suffix
+        left, right = i + 1, n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
