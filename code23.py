2ccd6de4e1fac12fa03e79d8b34f7aae2bd59fea
RutviKhunt <rutvikhunt2023@gmail.com>
2025-11-16 13:58:55 +0530

    solution of combination sum problem

---
+++ code23.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        candidates.sort()   # sorting helps with pruning
+
+        def backtrack(start, path, total):
+            if total == target:
+                result.append(path[:])
+                return
+            if total > target:
+                return
+
+            for i in range(start, len(candidates)):
+                path.append(candidates[i])
+                backtrack(i, path, total + candidates[i])  # i (NOT i+1) because we can reuse
+                path.pop()
+
+        backtrack(0, [], 0)
+        return result
